6c932fef0adfcc79b96a0059818208d5f2f64eec
Ulaş Noyan <99445511+noyanulas@users.noreply.github.com>
2024-12-14 21:58:51 +0300

    Add files via upload

--- src/meshDrawer.js
+++ src/meshDrawer.js
@@ -171,7 +171,13 @@ void main()
 	// PLEASE DO NOT CHANGE ANYTHING ABOVE !!!
 	// Calculate the diffuse and specular lighting below.
 
+	// Diffuse lighting
+	diff = max(dot(normal, lightdir), 0.0);
 
+	// Specular lighting
+	vec3 viewDir = normalize(-fragPos); // Assume the camera is at the origin
+	vec3 reflectDir = reflect(-lightdir, normal);
+	spec = pow(max(dot(viewDir, reflectDir), 0.0), phongExp);
 
 	// PLEASE DO NOT CHANGE ANYTHING BELOW !!!
 	/////////////////////////////////////////////////////////////////////////////
